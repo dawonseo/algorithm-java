8868927a7f14403cf993939415dcb3ef2362c4be
Dawon Seo <85838334+dawonseo@users.noreply.github.com>
2023-08-28 20:06:38 +0900

    Add BaekJoon 2439

---
+++ BaekJoon2439.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BaekJoon2439 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 1; i < (N + 1); i++) {
+            sb.append(" ".repeat(N - i));
+            sb.append("*".repeat(i));
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
