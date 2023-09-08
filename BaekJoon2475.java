500f10e8db78286b47948641cda08fb7271999f1
Dawon Seo <85838334+dawonseo@users.noreply.github.com>
2023-09-08 12:46:22 +0900

    Add BaekJoon 2475

---
+++ BaekJoon2475.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BaekJoon2475 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int ans = 0;
+
+        for (int i = 0; i < 5; i++) {
+            ans += (int) Math.pow(Integer.parseInt(st.nextToken()), 2);
+            ans %= 10;
+        }
+        System.out.println(ans);
+    }
+}
