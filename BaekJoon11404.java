886f71dac5b6c866d4c90b89a7de0b45d0bfac86
Dawon Seo <85838334+dawonseo@users.noreply.github.com>
2024-12-26 13:57:21 +0900

    Add BaekJoon 11404 플로이드

---
+++ BaekJoon11404.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BaekJoon11404 {
+    static int N, M;
+    static int[][] dist;
+    static final int INF = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+
+        dist = new int[N + 1][N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            Arrays.fill(dist[i], INF);
+            dist[i][i] = 0;
+        }
+        StringTokenizer st;
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+
+            dist[start][end] = Math.min(cost, dist[start][end]);
+        }
+
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    if (dist[i][k] != INF && dist[k][j] != INF) {
+                        dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                    }
+                }
+            }
+        }
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (dist[i][j] == INF) {
+                    sb.append("0 ");
+                } else {
+                    sb.append(dist[i][j]).append(" ");
+                }
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
