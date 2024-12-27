09020cde141dcdcf1d43c3af92c5e3e94afc38ca
Dawon Seo <85838334+dawonseo@users.noreply.github.com>
2024-12-27 19:19:21 +0900

    Add BaekJoon 14938 서강그라운드

---
+++ BaekJoon14938.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BaekJoon14938 {
+    static final int INF = 1000000000;
+    static int[] items;
+    static int ans = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+
+        int[][] dist = new int[n + 1][n + 1];
+        items = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            Arrays.fill(dist[i], INF);
+            dist[i][i] = 0;
+        }
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 1; i <= n; i++) {
+            items[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < r; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+
+            dist[u][v] = w;
+            dist[v][u] = w;
+        }
+
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (dist[i][k] != INF && dist[k][j] != INF) {
+                        dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                    }
+                }
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            int sum = 0;
+            for (int j = 1; j <= n; j++) {
+                if (dist[i][j] <= m) {
+                    sum += items[j];
+                }
+            }
+            ans = Math.max(ans, sum);
+        }
+
+        System.out.println(ans);
+    }
+}
